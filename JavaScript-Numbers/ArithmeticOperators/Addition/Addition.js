6198c31495d9b5ee028588f4ecfa384bd9b4e3a4
likithadavala59 <likithadavala59@gmail.com>
2026-07-27 21:40:32 +0530

    Adding Numbers

--- JavaScript-Numbers/ArithmeticOperators/Addition/Addition.js
+++ JavaScript-Numbers/ArithmeticOperators/Addition/Addition.js
@@ -1,5 +1,6 @@
 function onClickAddition() {
     debugger;
+    console.log("onClickAddition called");
     var firstNumber = Number(document.getElementById("txtFirstNumber").value);
     var secondNumber = Number(document.getElementById("txtSecondNumber").value);
     var addResult = firstNumber + secondNumber;
