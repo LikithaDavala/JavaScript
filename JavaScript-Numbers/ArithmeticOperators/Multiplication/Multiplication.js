014e03417c625bea088c49b72d87d96a2de24457
LikithaDavala <likithadavala59@gmail.com>
2026-07-29 21:06:47 +0530

    Multiplication ofm two numbers

--- JavaScript-Numbers/ArithmeticOperators/Multiplication/Multiplication.js
+++ JavaScript-Numbers/ArithmeticOperators/Multiplication/Multiplication.js
@@ -2,6 +2,6 @@ function onClickMultiplication() {
     debugger;
     var firstNumber = Number(document.getElementById("txtFirstNumber").value);
     var secondNumber = Number(document.getElementById("txtSecondNumber").value);
-    var mulResult = firstNumber + secondNumber;
+    var mulResult = firstNumber * secondNumber;
     document.getElementById("pResult").innerHTML = mulResult;
 }
