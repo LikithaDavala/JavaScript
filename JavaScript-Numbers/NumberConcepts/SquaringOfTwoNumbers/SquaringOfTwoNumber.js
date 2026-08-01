abf24abf87f9963aad8b8edb0d46250a3aa8c511
LikithaDavala <likithadavala59@gmail.com>
2026-08-01 18:06:30 +0530

    Squaring of two numbers

---
+++ JavaScript-Numbers/NumberConcepts/SquaringOfTwoNumbers/SquaringOfTwoNumber.js
@@ -0,0 +1,5 @@
+function onClickSquaringOfTwoNumber(){
+    var inputNumber =Number(document.getElementById("txtInputNumber").value);
+    var square = inputNumber*inputNumber;
+    alert(square);
+}
