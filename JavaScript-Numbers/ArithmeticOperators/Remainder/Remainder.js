fdbfe4fb1d28e1c22d2697d1a617b2b265fe4fe4
LikithaDavala <likithadavala59@gmail.com>
2026-08-04 21:48:45 +0530

    Remainder in Arithmetic Operator

--- JavaScript-Numbers/ArithmeticOperators/Remainder/Remainder.js
+++ JavaScript-Numbers/ArithmeticOperators/Remainder/Remainder.js
@@ -2,6 +2,6 @@ function onClickRemainder() {
     debugger;
     var firstNumber = Number(document.getElementById("txtFirstNumber").value);
     var secondNumber = Number(document.getElementById("txtSecondNumber").value);
-    var remResult = firstNumber + secondNumber;
+    var remResult = firstNumber % secondNumber;
     document.getElementById("pResult").innerHTML = remResult;
 }
