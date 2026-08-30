3b3b4774d32cb6d22d38b0311363b5359ef0a521
LikithaDavala <likithadavala59@gmail.com>
2026-08-30 22:07:39 +0530

    Adding two Numbers

--- JavaScript-Numbers/ArithmeticOperators/Addition/Addition.js
+++ JavaScript-Numbers/ArithmeticOperators/Addition/Addition.js
@@ -1,8 +1,20 @@
-function onClickAddition() {
+// function onClickAddition() {
+//     debugger;
+//     console.log("onClickAddition called");
+//     var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+//     var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+//     var addResult = firstNumber + secondNumber;
+//     document.getElementById("pResult").innerHTML = addResult;
+// }
+
+
+let _btnAddition = document.getElementById("btnAddition");
+
+_btnAddition.addEventListener("click",function(){
     debugger;
     console.log("onClickAddition called");
     var firstNumber = Number(document.getElementById("txtFirstNumber").value);
     var secondNumber = Number(document.getElementById("txtSecondNumber").value);
     var addResult = firstNumber + secondNumber;
     document.getElementById("pResult").innerHTML = addResult;
-}
+});
