5c2cfa4eb797b7dd66ac5caee000b69495088185
LikithaDavala <likithadavala59@gmail.com>
2026-08-31 22:30:12 +0530

    Remaindering the Two Numbers

--- JavaScript-Numbers/ArithmeticOperators/Remainder/Remainder.js
+++ JavaScript-Numbers/ArithmeticOperators/Remainder/Remainder.js
@@ -5,3 +5,9 @@ function onClickRemainder() {
     var remResult = firstNumber % secondNumber;
     document.getElementById("pResult").innerHTML = remResult;
 }
+
+
+
+let  _btnRemainder= document.getElementById("btnRemainder");
+
+_btnRemainder.addEventListener("click", onClickRemainder);
