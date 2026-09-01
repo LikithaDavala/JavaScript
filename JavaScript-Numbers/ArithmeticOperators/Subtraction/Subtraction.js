92d5eed34fc328485bd83479f07395be1db4648f
LikithaDavala <likithadavala59@gmail.com>
2026-09-01 23:13:32 +0530

    Subtraction

--- JavaScript-Numbers/ArithmeticOperators/Subtraction/Subtraction.js
+++ JavaScript-Numbers/ArithmeticOperators/Subtraction/Subtraction.js
@@ -10,4 +10,10 @@ function onClickSubtraction() {
 
 let _btnSubtraction = document.getElementById("btnSubtraction");
 
-_btnSubtraction.addEventListener("click", onClickSubtraction);
+_btnSubtraction.addEventListener("click", function {
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var subResult = firstNumber - secondNumber;
+    document.getElementById("pResult").innerHTML = subResult;
+});
