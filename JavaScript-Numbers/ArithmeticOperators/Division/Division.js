968167dfec2671d6051f9a1459dc5b4af19e070f
LikithaDavala <likithadavala59@gmail.com>
2026-09-01 23:13:07 +0530

    Division

--- JavaScript-Numbers/ArithmeticOperators/Division/Division.js
+++ JavaScript-Numbers/ArithmeticOperators/Division/Division.js
@@ -1,12 +1,19 @@
-function onClickDivision() {
+// function onClickDivision() {
+//  debugger;
+//     var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+//     var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+//     var divResult = firstNumber / secondNumber;
+//     document.getElementById("pResult").innerHTML = divResult;
+// }
+
+
+let _btnDivision = document.getElementById("onClickDivision");
+
+_btnDivision.addEventListener("click", function {
  debugger;
     var firstNumber = Number(document.getElementById("txtFirstNumber").value);
     var secondNumber = Number(document.getElementById("txtSecondNumber").value);
     var divResult = firstNumber / secondNumber;
     document.getElementById("pResult").innerHTML = divResult;
 }
-
-
-let _btnDivision = document.getElementById("onClickDivision");
-
-_btnDivision.addEventListener("click", onClickDivision);
+);
