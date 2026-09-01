b8aadba33b6fcd39dd5382ff17def1638f87282e
LikithaDavala <likithadavala59@gmail.com>
2026-09-01 23:13:21 +0530

    Multiplication

--- JavaScript-Numbers/ArithmeticOperators/Multiplication/Multiplication.js
+++ JavaScript-Numbers/ArithmeticOperators/Multiplication/Multiplication.js
@@ -1,12 +1,19 @@
-function onClickMultiplication() {
-    debugger;
-    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
-    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
-    var mulResult = firstNumber * secondNumber;
-    document.getElementById("pResult").innerHTML = mulResult;
-}
+// function onClickMultiplication() {
+//     debugger;
+//     var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+//     var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+//     var mulResult = firstNumber * secondNumber;
+//     document.getElementById("pResult").innerHTML = mulResult;
+// }
 
 
 let  _btnMultiplication= document.getElementById("btnMultiplication");
 
-_btnMultiplication.addEventListener("click", onClickMultiplication);
+_btnMultiplication.addEventListener("click", function  {
+ debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var divResult = firstNumber / secondNumber;
+    document.getElementById("pResult").innerHTML = divResult;
+}
+);
