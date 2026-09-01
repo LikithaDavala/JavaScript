d536f8f99ac8dc1f9b974f44477fa73b109797da
LikithaDavala <likithadavala59@gmail.com>
2026-09-01 23:14:02 +0530

    Remainder

--- JavaScript-Numbers/ArithmeticOperators/Remainder/Remainder.js
+++ JavaScript-Numbers/ArithmeticOperators/Remainder/Remainder.js
@@ -1,13 +1,19 @@
-function onClickRemainder() {
-    debugger;
-    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
-    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
-    var remResult = firstNumber % secondNumber;
-    document.getElementById("pResult").innerHTML = remResult;
-}
+// function onClickRemainder() {
+//     debugger;
+//     var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+//     var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+//     var remResult = firstNumber % secondNumber;
+//     document.getElementById("pResult").innerHTML = remResult;
+// }
 
 
 
 let  _btnRemainder= document.getElementById("btnRemainder");
 
-_btnRemainder.addEventListener("click", onClickRemainder);
+_btnRemainder.addEventListener("click", function {
+    debugger;
+    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
+    var remResult = firstNumber % secondNumber;
+    document.getElementById("pResult").innerHTML = remResult;
+});
